7d680377b7140ac4eac48245acf6b816a0248bdc
Aleospiria <Alejoospi779@gmail.com>
2026-04-10 17:07:12 -0500

    Variable de entorno del Schema dado por el profesor leido en los modelos

--- models/formacion.py
+++ models/formacion.py
@@ -1,8 +1,14 @@
 from sqlalchemy import Column, Integer, String, Date
 from database import Base
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+SCHEMA = os.getenv("SCHEMA")
 
 class Formacion(Base):
     __tablename__ = "formacion"
+    __table_args__ = {"schema": SCHEMA}
 
     idFormacion = Column(Integer, primary_key=True, index=True)
     idPersona = Column(Integer)
--- models/personal.py
+++ models/personal.py
@@ -1,8 +1,14 @@
 from sqlalchemy import Column, Integer, String, Boolean
 from database import Base
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+SCHEMA = os.getenv("SCHEMA")
 
 class Personal(Base):
     __tablename__ = "personal"
+    __table_args__ = {"schema": SCHEMA}
 
     idPersona = Column(Integer, primary_key=True, index=True)
     idCargo = Column(Integer)
